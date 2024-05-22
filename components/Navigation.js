87d70fa70ff3e6b7d4c55843e1a63329971a3b85
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-22 14:18:02 +0900

    大枠

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { HiOutlineBars3 } from "react-icons/hi2";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <>
+      {/* web menu */}
+      <header>
+        <div>
+          <div>
+            {/* logo */}
+            <div>
+              <Link href={"/"}>
+                <Image
+                  src={"/assets/logo.png"}
+                  width={90}
+                  height={60}
+                  alt="ロゴ"
+                />
+              </Link>
+            </div>
+
+            {/* menu */}
+            <div>
+              <ul>
+                <li>
+                  <Link href={"/"}>ホーム</Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* button */}
+            <div>
+              <Link href={"/"}>申し込み</Link>
+
+              {/* for mobile */}
+              <button>
+                <HiOutlineBars3 />
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* mobile menu */}
+    </>
+  );
 };
 
 export default Navigation;
