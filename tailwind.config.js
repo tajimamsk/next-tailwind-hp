912768b339dee8d6ac17c06547090818c6b648cf
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-22 13:50:19 +0900

    初期設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        geen: "#00a497",
+        blue: "#00a3af",
+        heading: "#302833",
+        body: "#2b2b2b",
+        light: "#e7e7eb",
+        pinkLight: "#fdeff2",
+        yellowLight: "#f4dda5",
+        purpleLight: "#dbd0e6",
+        redLight: "#e597b2",
       },
     },
   },
