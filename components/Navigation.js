a0a88f46ea4443940a864df1c956937d55e24bd1
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-22 15:06:36 +0900

    menuの大枠

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,14 +2,39 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { HiOutlineBars3 } from "react-icons/hi2";
+import { IoClose } from "react-icons/io5";
+import { BiChevronRight } from "react-icons/bi";
+
+const navigationMenu = [
+  {
+    href: "#home",
+    label: "ホーム",
+  },
+  {
+    href: "#services",
+    label: "サービス",
+  },
+  {
+    href: "#solutions",
+    label: "ソリューション",
+  },
+  {
+    href: "#testiomonials",
+    label: "カスタマー",
+  },
+  {
+    href: "#blog",
+    label: "ブログ・ニュース",
+  },
+];
 
 const Navigation = () => {
   return (
     <>
       {/* web menu */}
-      <header>
-        <div>
-          <div>
+      <header className="py-7">
+        <div className="container px-4 mx-auto">
+          <div className="flex justify-between items-center">
             {/* logo */}
             <div>
               <Link href={"/"}>
@@ -23,21 +48,28 @@ const Navigation = () => {
             </div>
 
             {/* menu */}
-            <div>
-              <ul>
-                <li>
-                  <Link href={"/"}>ホーム</Link>
-                </li>
+            <div className="hidden lg:block text-center">
+              <ul className="flex space-x-7">
+                {navigationMenu.map((item, index) => (
+                  <li key={index} className="text-body">
+                    <Link href={item.href}>{item.label}</Link>
+                  </li>
+                ))}
               </ul>
             </div>
 
             {/* button */}
             <div>
-              <Link href={"/"}>申し込み</Link>
+              <Link
+                href={"#"}
+                className="btnBlue inline-flex lg:inline-block max-lg:hidden"
+              >
+                申し込み
+              </Link>
 
               {/* for mobile */}
-              <button>
-                <HiOutlineBars3 />
+              <button className="block lg:hidden">
+                <HiOutlineBars3 className="text-4xl" />
               </button>
             </div>
           </div>
@@ -45,6 +77,30 @@ const Navigation = () => {
       </header>
 
       {/* mobile menu */}
+      <div>
+        <div>
+          <div>
+            <div>
+              <button>
+                <IoClose />
+                <span>閉じる</span>
+              </button>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <Link href="">
+                    <span>ホーム</span>
+                    <span>
+                      <BiChevronRight />
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
