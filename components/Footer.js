45df21e110acac907f9098d0aad8a54883262337
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-29 14:22:19 +0900

    スタイル＋レスポンシブ

--- components/Footer.js
+++ components/Footer.js
@@ -9,10 +9,11 @@ import { IoIosLink } from "react-icons/io";
 const footerContent = {
   text: {
     logo: "/assets/logo.png",
-    description: "成績はもう上がらないと諦めていませんか？",
+    description:
+      "成績はもう上がらないと諦めていませんか？成績が上がらないのは能力のせいではありません。今までのやり方を変えるだけで、学力・成績は短期間で飛躍的に伸びます！",
     footerLinks: [
       {
-        heading: "",
+        heading: "概要",
         links: [
           {
             href: "#",
@@ -60,41 +61,61 @@ const footerContent = {
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div>
+    <footer className="py-20 bg-white max-md:p-5">
+      <div className="container px-4 mx-auto">
+        <div className="block lg:flex gap-20 mb-10 pb-10 text-body">
           {/* 左 */}
-          <div>
+          <div className="w-full lg:w-4/12 mb-10 lg:mb-0">
             <Link href={"#"}>
               <Image
                 src={footerContent.text.logo}
                 width={90}
                 height={60}
                 alt="logo"
+                className="mb-5"
               />
             </Link>
-            <p>{footerContent.text.description}</p>
-            <Link href={"#"}>
+            <p className="leading-relaxed mb-7">
+              {footerContent.text.description}
+            </p>
+            <Link
+              href={"#"}
+              className="flex space-x-2 outline-none items-center 
+              font-semibold  text-green-500 group"
+            >
               <span>もっと見る</span>
-              <span>
-                <BiChevronRight />
+              <span
+                className="w-6 h-6 rounded-full bg-green-500 text-white 
+              inline-flex items-center justify-center group-hover:bg-[#006E54]
+              duration-300 transition-all ease-in-out"
+              >
+                <BiChevronRight className="text-xl" />
               </span>
             </Link>
           </div>
 
           {/* 中 */}
-          <div>
-            <div>
+          <div className="w-full lg:w-4/12 mb-10 lg:mb-0">
+            <div className="grid grid-cols-2 gap-10">
               {footerContent.text.footerLinks.map((footerLink, index) => (
                 <div key={index}>
-                  <h3>{footerLink.heading}</h3>
-                  <ul>
+                  <h3 className="font-semibold text-heading mb-5">
+                    {footerLink.heading}
+                  </h3>
+                  <ul className="p-0 m-0">
                     {footerLink.links.map((link, index) => (
-                      <li key={index}>
-                        <Link href={"#"}>
-                          <span>{link.label}</span>
-                          <span>
-                            <BiChevronRight />
+                      <li key={index} className="mb-3">
+                        <Link
+                          href={"#"}
+                          className="flex items-center duration-300 transition-all 
+                          ease-in-out hover:text-green-500 group"
+                        >
+                          <span className="max-md:text-xs">{link.label}</span>
+                          <span
+                            className="left-2 relative duration-200 transition-all 
+                            ease-in-out opacity-0 group-hover:opacity-100 group-hover:left-3"
+                          >
+                            <BiChevronRight className="text-xl" />
                           </span>
                         </Link>
                       </li>
@@ -106,26 +127,33 @@ const Footer = () => {
           </div>
 
           {/* 右 */}
-          <div>
-            <h3>{footerContent.contact.heading}</h3>
-            <p>{footerContent.contact.description}</p>
+          <div className="w-full lg:w-4/12 mb-10 lg:mb-0">
+            <h3 className="font-semibold text-heading mb-5">
+              {footerContent.contact.heading}
+            </h3>
+            <p className="leading-relaxed mb-7">
+              {footerContent.contact.description}
+            </p>
             <ul>
-              <li>
-                <FaLocationDot />
+              <li className="flex items-start space-x-3 mb-5">
+                <FaLocationDot className="text-xl text-green-500" />
                 <span>{footerContent.contact.address.street}</span>
               </li>
-              <li>
+              <li className="flex items-start space-x-3 mb-5">
                 <FaPhoneAlt />
                 <span>p{footerContent.contact.address.phone}</span>
               </li>
-              <li>
+              <li className="flex items-start space-x-3 mb-5">
                 <IoIosLink />
                 <span>{footerContent.contact.address.street}</span>
               </li>
             </ul>
           </div>
         </div>
-        <div>Copyright ORIGIN.Doc 2023</div>
+
+        <div className="text-center pt-10 border-t border-gray-200">
+          Copyright ORIGIN.Doc 2023
+        </div>
       </div>
     </footer>
   );
